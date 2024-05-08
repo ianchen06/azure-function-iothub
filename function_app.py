7e2ae940182fcd405c16cc42f5e51f8d578d6075
Ian Chen <ianchen06@gmail.com>
2024-05-07 22:00:01 -0700

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -9,8 +9,8 @@ con = psycopg2.connect(os.getenv("DATABASE_URL"))
 
 @app.event_hub_message_trigger(
     arg_name="azeventhub",
-    event_hub_name="iothub-ehub-techin510-59467401-6d881a7ae1",
-    connection="IOHUB_CONNECTION_STRING",
+    event_hub_name="iothub-ehub-techin510-59467401-6d881a7ae1", #FIXME: Change to your own iothub event_hub_name
+    connection="IOTHUB_CONNECTION_STRING", #Remember to add the IOTHUB_CONNECTION_STRING environment variable in your Function App
 )
 def eventhub_trigger1(azeventhub: func.EventHubEvent):
     logging.info(
